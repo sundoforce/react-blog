84efd4345a1b9651ca2432fcdda93775c5600595
sdk <admin@sundo.kim>
2020-12-01 19:06:33 +0900

    post.ctrl.js

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,24 @@
+// https://thebook.io/080203/ch21/05/05/02-02/
+let postId = 1;
+
+// posts 배열 init
+const posts = [
+    {
+        id: 1, 
+        title: '제목',
+        body: '내용'
+    },
+];
+
+/* 포스트 작성
+ POST /api/posts
+ {title, body}
+*/
+
+exports.write = ctx => {
+    const { titie}
+}
+
+
+// TODO 
+// https://thebook.io/080203/ch21/05/05/02-02/
