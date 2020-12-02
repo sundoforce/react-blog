c2746d5dfea739194ac5ffa9116854016b4dc099
sdk <admin@sundo.kim>
2020-12-02 19:00:24 +0900

    mongoDb

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -3,12 +3,10 @@ const postsCtrl = require('./posts.ctrl');
 
 const posts = new Router();
 
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
 posts.patch('/:id', postsCtrl.update);
 
 export default posts;
