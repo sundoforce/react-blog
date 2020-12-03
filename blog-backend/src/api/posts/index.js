d65c56d6bcc47ea20335d47cd4a38f50b6f338af
sdk <admin@sundo.kim>
2020-12-03 23:17:24 +0900

    before posts.ctrl.js

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,5 +1,5 @@
-const Router = require('koa-router');
-const postsCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
@@ -8,5 +8,6 @@ posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
 posts.patch('/:id', postsCtrl.update);
+
 
 export default posts;
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,35 +1,5 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
+/* eslint-disable no-global-assign */
 
-const { PORT, MONGO_URI } = process.env;
-
-mongoose
-.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-.then(() => {
-  console.log('Connected to MongoDB')
-  })
-  .catch(e => {
-   console.error(e)
-})
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-// 라우터 설정
-router.use('/api', api.routes()); // api 라우트 적용
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('listening to port %d', port);
-});
-
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
