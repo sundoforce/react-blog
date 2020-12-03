fa5501935bfc7586dc73f734ecb046c75750e9ba
sdk <admin@sundo.kim>
2020-12-03 23:55:23 +0900

    modify update

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -126,32 +126,32 @@ export const remove = async ctx => {
 */
 export const update = async ctx => {
     const { id } = ctx.params;
-    // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+    // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
     const schema = Joi.object().keys({
-        tilte: Joi.string(),
-        body: Joi.string(),
-        tags: Joi.array().items(Joi.string()),
-    })
-
-    // 검증하고 나서 검증 실패인 경우 에러 처리
-    const result = Joi.validate(ctx.request.body, schema);
+      title: Joi.string(),
+      body: Joi.string(),
+      tags: Joi.array().items(Joi.string()),
+    });
+  
+    // 검증 후, 검증 실패시 에러처리
+    const result = schema.validate(ctx.request.body);
     if (result.error) {
-        ctx.status = 400; // bad Request
-        ctx.body = result.error;
-        return;
+      ctx.status = 400; // Bad Request
+      ctx.body = result.error;
+      return;
     }
-
+  
     try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-            new: true, // 이 값을 설정하면 업데이트된 데이터를 반홯빈다.
-            // false 일 때는 업데이트되기 전의 데이터를 반환합ㄴ디ㅏ.
-        }).exec();
-        if (!post) {
-            ctx.status = 404;
-            return;
-        }
-        ctx.body = post;
+      const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+        new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+        // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
+      }).exec();
+      if (!post) {
+        ctx.status = 404;
+        return;
+      }
+      ctx.body = post;
     } catch (e) {
-        ctx.throw(500, e);
+      ctx.throw(500, e);
     }
-};
+  };
