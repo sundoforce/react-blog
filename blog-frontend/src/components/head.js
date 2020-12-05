337eef5596a351186acef1b214f3ea8992bbbcf2
sdk <admin@sundo.kim>
2020-12-05 19:56:40 +0900

    feat : login ui

---
+++ blog-frontend/src/components/head.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const headBlock = styled.div``;
+
+const head = () => {
+  return (
+    <headBlock>
+      
+    </headBlock>
+  );
+};
+
+export default head;
--- blog-frontend/src/pages/LoginPage.js
+++ blog-frontend/src/pages/LoginPage.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
+
 
 const LoginPage = () => {
-    return <div>로그인</div>
+    return (
+        <AuthTemplate>
+            <AuthForm type="login" />
+        </AuthTemplate>
+    )
 };
 
 export default LoginPage;
+
