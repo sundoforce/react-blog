8838ffec248683623a12508589563bb5b50c3ca9
sdk <admin@sundo.kim>
2020-12-05 13:39:03 +0900

    토큰 middle ware username print

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -90,7 +90,13 @@ export const login = async ctx => {
 };
 
 export const check = async ctx => {
-    // 로그인 상태 확인
+    const { user } = ctx.state;
+    if (!user) {
+        // 로그인 중 아님
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+    ctx.body = user;
 };
 
 export const logout = async ctx => {
