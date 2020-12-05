6428554bfe2b3c600042aa8cbc61c708638f6b80
sdk <admin@sundo.kim>
2020-12-05 14:07:27 +0900

    if post delete, update, -> login check

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,14 +4,25 @@ import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
     if (!ObjectId.isValid(id)) {
         ctx.status = 400;
         return;
     }
-    return next();
-}
+    try {
+      const post = await Post.findById(id);
+      // 포스트가 존재 하지 않을 때
+      if (!post) {
+        ctx.status = 404; // Not Found
+        return;
+      }
+      ctx.state.post = post;
+      return next();
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+};
 
 /*
   POST /api/posts
@@ -44,6 +55,7 @@ export const write = async ctx => {
       title,
       body,
       tags,
+      user: ctx.state.user,
     });
     try {
       await post.save();
@@ -90,18 +102,17 @@ export const list = async ctx => {
   GET /api/posts/:id
 */
 export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404; // Not Found
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
+}
 
 /*
   DELETE /api/posts/:id
