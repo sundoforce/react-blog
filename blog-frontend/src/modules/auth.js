9d1ee4648509ba6a2d71c6498674ab87fad4b481
sdk <admin@sundo.kim>
2020-12-05 20:55:33 +0900

    24-1

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,9 +1,31 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { takeLatest } from 'redux-sage/effects';
+import createRequestSaga, {
+    createRequestActinoTypes,
+} from '../lib/createRequestSaga'
+import * as authAPI from '../lib/api/auth';
 
 
 const CHANGE_FIELD = 'auth/CHANG_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+    'auth/REGISTER',
+)
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE ] = createRequestActionTypes(
+    'auth/LOGIN'
+)
+
+const REGISTER = 'auth/REGISTER';
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+const LOGIN = 'auth/LOGIN';
+const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -15,29 +37,65 @@ export const changeField = createAction(
 );
 
 export const initializeForm = createAction(INITIALIZE_FORM, form => form);//regiset, login
+export const register = createAction(REGISTER, ({ username, password }) => ({
+    username,
+    password,
+}));
+export const login = createAction(LOGIN, ({ username, password}) => ({
+    username,
+    password,
+}))
+
+// 사가 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function *authSage() {
+    yield takeLatest(REGISTER, registerSage);
+    yield takeLatest(LOGIN, loginSaga);
+}
 
 const initialState = {
     register: {
-        username: '',
+        username:'',
         password: '',
-        passwordConfirm: '',
+        passwordConfirm:'',
     },
-    login: {
+    login:{
         username: '',
         password: '',
-    }
+    },
+    auth: null,
+    authError: null,
 }
 
 const auth = handleActions(
     {
-    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
-    produce(state, draft => {
-      draft[form][key] = value; // 예 state.register.username을 바꾼다.
-    }),
-    [INITIALIZE_FORM] : (state, { payload: form }) => ({
-        ...state,
-        [form]: initialState[form],
-    }),
+        [CHANGE_FIELD]: (state, {payload: form}) => 
+            produce(state, draft => {
+              draft[form][key] = value;  
+            }),
+        [INITIALIZE_FORM]: (state, {payload: form}) => ({
+            ...state,
+            [form]: initialState[form],
+            authError: null, // 폼 전환시 회원인증 에러 초기화
+            }),
+            // 회원 가입 성공
+        [REGISTER_SUCCESS] : (state, { payload: auth}) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        // 회원가입 실패
+        [REGISTER_FAILURE] : (state, {payload: error }) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        // 로그인 실패
+        [LOGIN_FAILURE]: (state, {payload: error }) => ({
+            ...state,
+            authError: error,
+        })
     },
     initialState,
 );
--- blog-frontend/src/modules/index.js
+++ blog-frontend/src/modules/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
 import auth from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
     auth,
+    loading,
 });
+
+export function* rootSage() {
+    yield all([authSaga()]);
+}
 
 export default rootReducer;
