5fd2dcb12f5fa467d5a7dae5ef24501d829bb1de
sdk <admin@sundo.kim>
2020-12-07 23:29:25 +0900

    feat: chatper 25: write

--- blog-frontend/src/modules/write.js
+++ blog-frontend/src/modules/write.js
@@ -1,19 +1,43 @@
 import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+    createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
 
 const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기회 
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; // 특정 key 값 바꾸기
+
+const [
+    WRITE_POST,
+    WRITE_POST_SUCCESS,
+    WRITE_POST_FAILURE,
+] = createRequestActionTypes('write/WRITE_POST'); // 포트스 작성 
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
     key,
     value,
-}))
+}));
+export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
+    title,
+    body,
+    tags,
+}));
+
+// 사가 생성 
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+export function* writeSaga() {
+    yield takeLatest(WRITE_POST, writePostSaga);
+};
 
 
 const initialState = {
     title: '',
     body: '',
     tags: [],
+    post: null,
+    postError: null,
 };
 
 const write = handleActions(
@@ -23,6 +47,23 @@ const write = handleActions(
             ...state,
             [key]: value, // 특정 key값을 업데이 
         }),
+        [WRITE_POST]: state => ({
+
+            ...state,
+            // post와 postError 를 초기회 
+            post: null,
+            postError: null,
+        }),
+        // 포스트 작성 성공
+        [WRITE_POST_SUCCESS]: (state, { payload: post}) => ({
+            ...state,
+            post,
+        }),
+        // 포스트 작성 실패
+        [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
+            ...state,
+            postError,
+        })
     },
     initialState,
 );
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 
 const WritePage = () => {
@@ -10,7 +10,7 @@ const WritePage = () => {
         <Responsive>
             <EditorContainer />
             <TagBoxContainer />
-            <WriteActionButtons />
+            <WriteActionButtonsContainer />
         </Responsive>
     )
 };
