60481daaf3a06e83a3fd2365a4d9201f88d9b24a
sdk <admin@sundo.kim>
2020-12-12 12:05:08 +0900

    feat: helmet

--- blog-frontend/src/components/post/PostViewer.js
+++ blog-frontend/src/components/post/PostViewer.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
 import SubInfo from '../common/SubInfo';
+import { Helmet } from 'react-helmet-async';
 
 const PostViewerBlock = styled(Responsive) `
   margin-top: 4rem;
@@ -40,6 +41,9 @@ const PostContent = styled.div`
       const { title, body, user, publishedDate } = post;
       return (
           <PostViewerBlock>
+              <Helmet>
+                  <title>{title} - REACTERS </title>
+              </Helmet>
               <PostHead>
                   <h1>{title}</h1>
                    <SubInfo
--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -5,6 +5,8 @@ import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 import PostActionButtons from '../../components/post/PostActionButtons';
 import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
+
 
 const PostViewerContainer = ({ match, history }) => {
     // 처음으로 마운트될 때 포스트 읽기 API 요청
@@ -30,11 +32,21 @@ const PostViewerContainer = ({ match, history }) => {
         history.push('/write');
     };
 
+    const onRemove = async () => {
+        try {
+            await removePost(postId);
+            history.push('/'); // 홈으로 이동 
+        } catch (e) {
+            console.log(e);
+        };
+    };
+
     return <PostViewer 
       post={post} 
       loading={loading} 
       error={error} 
-      actionButtons={<PostActionButtons onEdit={onEdit}
+      actionButtons={<PostActionButtons onEdit={onEdit} 
+        onRemove={onRemove} 
         ownPost={user && user.id === post && post.id}
       />} 
      />;
