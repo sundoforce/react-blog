46249c8315a04ab11834d52777cf327b381c37d9
sdk <admin@sundo.kim>
2020-12-26 00:01:55 +0900

    fix bug

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -8,7 +8,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
-// import { tempSetUser, check } from './modules/user'
+import { tempSetUser, check } from './modules/user'
 import { HelmetProvider } from 'react-helmet-async';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -18,17 +18,20 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
-/*
 function loadUser() {
-   const user = localStorage.getItem('user');
-   if (!user) return; // 로그인 상태가 아니라면 아무것도 안함.
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
 
-   store.dispatch(tempSetUser(user));
-   store.dispatch(check());
-
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
 }
-*/
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
