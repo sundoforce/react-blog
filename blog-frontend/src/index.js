be3de0990345a487a89668ca0010cb6d5968c11e
sdk <admin@sundo.kim>
2020-12-25 23:55:06 +0900

    fix wrraing

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -8,7 +8,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
-import { tempSetUser, check } from './modules/user'
+// import { tempSetUser, check } from './modules/user'
 import { HelmetProvider } from 'react-helmet-async';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -18,6 +18,7 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
+/*
 function loadUser() {
    const user = localStorage.getItem('user');
    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함.
@@ -26,6 +27,7 @@ function loadUser() {
    store.dispatch(check());
 
 }
+*/
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
