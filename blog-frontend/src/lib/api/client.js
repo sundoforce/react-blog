1dc6160d0fd6fc9d4f91ae358cd602bbae1c9259
sdk <admin@sundo.kim>
2021-01-26 23:40:38 +0900

    sss

--- blog-frontend/src/lib/api/client.js
+++ blog-frontend/src/lib/api/client.js
@@ -6,8 +6,9 @@ const client = axios.create();
   글로벌 설정 예시:
   
   // API 주소를 다른 곳으로 사용함
-  client.defaults.baseURL = 'https://external-api-server.com/' 
-
+  */
+  client.defaults.baseURL = 'http://192.168.55.71:4000/' 
+/*
   // 헤더 설정
   client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
 
