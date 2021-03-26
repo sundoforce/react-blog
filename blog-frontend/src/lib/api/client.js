9b00ca15df3f5ee1140d187ea4a1128846afd541
sdk <admin@sundo.kim>
2021-03-26 19:38:08 +0900

    axios cors

--- blog-frontend/src/lib/api/client.js
+++ blog-frontend/src/lib/api/client.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 const client = axios.create();
+const cors = require('cors');
 
 /*
   글로벌 설정 예시:
   
   // API 주소를 다른 곳으로 사용함
   */
-  client.defaults.baseURL = 'http://192.168.55.71:4000/' 
+client.defaults.baseURL = 'http://192.168.55.71:4000/';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 /*
   // 헤더 설정
   client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
